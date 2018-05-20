9280baa8932d5dc813b8c641e36029fdfa20a8da
guilhermealveslopes <guilherme_alves-rs@hotmail.com>
2018-05-20 16:02:59 -0300

    Versão 2.0

---
+++ funcoes.js
@@ -0,0 +1,100 @@
+var iniciahtml = 
+'<!DOCTYPE html>'+
+'<head>'+
+    '<meta charset="utf-8"/>'+
+    '<title>Frame</title>'+
+    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>'+
+    '<script src="funcoes.js"></script>'+
+    '<link rel="stylesheet" href="zpixel.css">'+
+    '<link rel="stylesheet" href="style.css">'+
+'</head>'+
+'<body>';
+
+var terminahtml = 
+'</html>'+
+'</body>';
+    
+var relacionados = '<div class="relacionados">'+
+'<div class="item">'+
+    '<div class="img">'+
+        '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw4HRPVtZe64zCx5V1M6Y-kDoHZ1TYGawywK0xJ2uER0nqXPvlmg" alt="">'+
+    '</div>'+
+    '<h1>Agencia inteligente</h1>'+
+'</div>'+
+'<div class="item">'+
+   ' <div class="img">'+
+       ' <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw4HRPVtZe64zCx5V1M6Y-kDoHZ1TYGawywK0xJ2uER0nqXPvlmg" alt="">'+
+    '</div>'+
+   ' <h1>Z Pixel, muda-se para o campo</h1>'+
+'</div>'+
+'<div class="item">'+
+   '<div class="img">'+
+       '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw4HRPVtZe64zCx5V1M6Y-kDoHZ1TYGawywK0xJ2uER0nqXPvlmg" alt="">'+
+    '</div>'+
+   '<h1>Melhores desenvolvedores optam por trabalhar na Z Pixel</h1>'+
+'</div>'+
+'</div>';
+
+
+var sobre = '<!-- Sobre a Credipronto -->'+
+'<div class="sobre">'+
+   '<div class="container">'+
+        '<div class="coluna descricao">'+
+            '<div class="heading">'+
+                '<h2>Sobre a CrediPronto</h2>'+
+            '</div>'+
+            '<div class="info">'+
+                '<p>O itaú, maior intituição financeira do hemisfério sul, e a Lopes, a maior imobiliária do Brasil, se uniram para trazer um conceito inovador: oferecer o <strong>fincanciamento imobiliário que você precisa</strong>. Assim surgiu a CrediPronto. A única empresa especializada em <strong>consultoria de financiamento imobiliário</strong> do país. Conheça todos os benefícios e veja o porquê de fazer o financiamento do seu imível com a CrediPronto.</p>'+
+                '<button class="saiba-mais">'+
+                    '<i class="icon-arrow-right"></i> Saiba Mais'+
+                '</button>'+
+            '</div>'+
+        '</div>'+
+        '<div class="coluna video">'+
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/xW2DPUVZttg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'+
+        '</div>'+
+    '</div>'+
+'</div>';
+
+
+var depoimentos = '<!-- Depoimentos -->'+
+
+'<div class="depoimentos">'+
+    '<div class="container">'+
+        '<div class="heading">'+
+            '<h2>Depoimentos de Cliente e Parceiros</h2>'+
+        '</div>'+
+    '</div>'+
+        '<div class="box-depoimentos">'+
+            '<div class="container">'+
+                '<div class="item">'+
+                    '<div class="cliente">'+
+                        '<img src="img/cliente.jpg" alt="">'+
+                        '<h3>Cliente 2</h3>'+
+                    '</div>'+
+                    '<div class="depoimento">'+
+                        '<i class="icon-quote"></i>'+
+                        'Realizamos neste último mês a compra do nosso primeiro imóvel através do financiamento junto à Credipronto e ficamos surpreendidos'+
+                        'com a qualidade do serviço prestado. A empresa superou nossas expectativas em questões de praticidade e agilidade na análide'+
+                        'e rápida aprovação do financiamento, sem burocracia demais e com tacas competitivas que, com toda certeza, fizeram a diferença'+
+                        'e viabilizaram o fechamento do negóco e a realização deste grande sonho. No quesito atendimento, não podemos deixar de parabenizar e agradecer à Joana Vitale e Fábio de Azebedo'+
+                        'pelo atendimento totalmente diferenciado que nos foi prestado, sempre com muito empenho e seriedade, ambos foram atenciosos e pacientes'+
+                        'ao conduzir nossa negociação desde o primeiro contato até a conclusão, nos auxiliando no que foi preciso e posicionando passo a passo sobre como'+
+                        'estava o andamento do processo, o que nos deixou tranquilos neste momento tão importante. Estamos muito satisfeitos e recomendamos a credipronto!'+
+                        'Parabéns à toda a equipe!'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>';
+
+var noticias = '<!-- Últimas notícias -->'+
+'<div class="ultimas-noticias">'+
+    '<div class="container">'+
+        '<div class="chamada">'+
+            '<i class="icon-bars"></i> Confira as nossas últimas notícias'+
+        '</div>'+
+        '<a href="#">Acesse nosso blog</a>'+
+    '</div>'+
+'</div>';
