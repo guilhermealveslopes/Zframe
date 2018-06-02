dca8034f0c21db64b9263f8f71a87d6f99a20bd1
guilhermealveslopes <guilhermealves.rs@gmail.com>
2018-06-02 13:45:00 -0300

    Fix diretório inicial, Multiplos blocos por pasta, bloco sem conteúdo

--- lista-de-blocos.php
+++ lista-de-blocos.php
@@ -1,6 +1,12 @@
-<?php 
-   $isFolder = is_dir("/opt/lampp/htdocs/Zframe/blocos_html"); //Define a pasta dos blocos HTML
-   $folder = "/opt/lampp/htdocs/Zframe/blocos_html";  //Define a pasta dos blocos HTML
+<?php
+
+$arquivos = get_included_files();
+$root = $arquivos[1];
+$split = explode('lista-de-blocos.php', $root);
+$HTML_ROOT = $split[0] . '/blocos_html';
+
+   $isFolder = is_dir($HTML_ROOT); //Define a pasta dos blocos HTML
+   $folder = $HTML_ROOT;  //Define a pasta dos blocos HTML
    if($isFolder){
       $data = scandir($folder); // Scabeia a pasta e procura pelos tipos de blocos [header] [footer] [etc]
    } 
@@ -8,6 +14,7 @@
    unset($data[1]); // Remove o atalho para as pastas anteriores [../]
    if($data){
       foreach ($data as $dat) {
+        echo '<div class="bloco material-'.$dat.'">';
             // Para cada pasta encontrada, repete o processo para encontrar os arquivos internos [credi.html]
             $sub_folder = $folder . '/' . $dat;
             $info = pathinfo($folder."/".$dat); 
@@ -15,25 +22,42 @@
             unset($sub_scan[0]);
             unset($sub_scan[1]);
             if(!empty($sub_scan)){
+               foreach($sub_scan as $blocos){
+                   
+                $bloco = str_replace('.html', '', $blocos); // Remove a extensão .html para usar o nome do block [credi]
+                $imgdir = scandir('blocos_img/'.$dat);
+                if(in_array($bloco.'.png', $imgdir)){
+                    $img = true;
+                }
+                else{
+                    $img = false;
+                }
+                
+                ?>
             
-            $bloco = str_replace('.html', '', $sub_scan[2]); // Remove a extensão .html para usar o nome do block [credi]
-            ?>
-            <!-- Cria lista de blocos disponíveis -->
-            <div class="bloco material-<?php print_r($dat) ?>">
+                <!-- Cria lista de blocos disponíveis -->
                 <li>
-                    <img src="blocos_img/<?php print_r($dat) ?>/<?php print_r($bloco) ?>.png" alt="">
+                    <?php if($img):  echo '<img src="blocos_img/'.$dat.'/'.$bloco.'.png" alt="">'; ?>
+                    <?php else:  echo '<img src="img/semimg.png" alt="">'; endif; ?>
                     <div class="controles">
-                        <a class="swipebox" href="#<?php print_r($dat) ?>-<?php print_r($bloco) ?>">Visualizar</a>
+                        <?php if($img):?>
+                        <a href="#<?php print_r($dat) ?>-<?php print_r($bloco) ?>">Visualizar</a>
                         <a class="addblock" data-block="<?php print_r($bloco) ?>" type-block="<?php print_r($dat) ?>" href="#">Adicionar</a>
+
+                        <?php else:?>
+                        <a href="#<?php print_r($dat) ?>-<?php print_r($bloco) ?>">Visualizar</a>
+                        <a href="#<?php print_r($dat) ?>-<?php print_r($bloco) ?>">Adicionar</a>
+                        <?php endif; ?>
                     </div>
                     <a href="#_" class="lightbox" id="<?php print_r($dat) ?>-<?php print_r($bloco) ?>">
-                        <img src="blocos_img/<?php print_r($dat) ?>/full/<?php print_r($bloco) ?>.png" alt="">
+                        <?php if($img):  echo '<img src="blocos_img/'.$dat.'/full/'.$bloco.'.png" alt="">'; ?>
+                        <?php else:  echo '<div class="alerta"><span>Bloco não disponível, volte em breve.</span></div>'; endif; ?>
                     </a>
                 </li>
-            </div>
-
-            <?php 
+                <?php 
+               }
             }
+        echo '</div>';
         };
     }else{
       
