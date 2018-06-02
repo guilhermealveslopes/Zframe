0bc50d95816ea042cc99d2ce503e852adf938c1c
guilhermealveslopes <guilhermealves.rs@gmail.com>
2018-06-02 20:34:21 -0300

    Renomeia img de acordo com o bloco

--- importar.php
+++ importar.php
@@ -2,10 +2,7 @@
 $bloco_nome = $_POST["bloco_nome"];
 $bloco_tipo = $_POST["bloco_tipo"];
 $bloco_html = $_POST["bloco_html"];
-$bloco_css = $_POST["bloco_css"];
-
-
-
+$bloco_css  = $_POST["bloco_css"];
 
 #Criar o arquivo HTML
 $fp = fopen("blocos_html/" . $bloco_tipo . "/" . $bloco_nome .".html", "w");
@@ -15,21 +12,21 @@ $fw = fwrite($fp, $bloco_html);
 $fp = fopen("blocos_css/" . $bloco_tipo . "/" . $bloco_nome .".css", "w");
 $fw = fwrite($fp, $bloco_css);
 
-
-
 $target_dir = "blocos_img/" . $bloco_tipo . "/";
 $target_file = $target_dir . basename($_FILES["bloco_img"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+$extensao = explode(".", $_FILES["bloco_img"]["name"]);
+$img = $bloco_nome . '.' . end($extensao);
+
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["bloco_img"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        move_uploaded_file($_FILES["bloco_img"]["tmp_name"], $target_file);
+        echo 'Novo bloco ['.$bloco_nome.'] criado com sucesso!';
+        move_uploaded_file($_FILES["bloco_img"]["tmp_name"], $target_dir.$img);
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "Anexo inválido.";
         $uploadOk = 0;
     }
 }
